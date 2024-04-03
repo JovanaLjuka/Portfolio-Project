b05ae3efada6554bbad1ab9628ce3e5c3fe05105
Jovana Ljuka <jovanaljuka1@gmail.com>
2024-04-03 13:18:49 +0200

    Created basic project structure in app.jsx, created scss file, implemented some general styles

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss'
+
+function App() {
+  return (
+    <main>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>About</section>
+      <section>Parallax</section>
+      <section>Skills</section>
+      <section>Parallax</section>
+      <section>Projects</section>
+      <section>Contact</section>
+    </main>
+  )
+}
+
+export default App
